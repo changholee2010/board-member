aadda71cb4f030afa911246b730c1d0a262e0a0c
funfun <fun@yedam.ac>
2026-01-30 12:32:30 +0900

    f

--- backend/controllers/member.controller.js
+++ backend/controllers/member.controller.js
@@ -1,14 +1,24 @@
 const service = require("../services/member.service");
+const crypto = require("crypto");
+
 // req, res => 활용해서 기능.
 const ctrl = {
-  register: (req, res) => {
+  register: async (req, res) => {
     const { user_id, user_pw, user_name } = req.body;
     // 등록.
     // user_id, user_pw, user_name
-    const result = service.register({ user_id, user_pw, user_name });
-    console.log(result);
+    let passwd = crypto.createHash("sha512").update(user_pw).digest("base64");
+    // 위에서 평문을 암호화한 값으로 변경한 후 등록.
+
     // 응답.
-    res.send("처리완료");
+    try {
+      const result = await service.register({ user_id, passwd, user_name });
+      console.log(result);
+      res.json({ retCode: "OK" });
+    } catch (err) {
+      console.log(err);
+      res.json({ retCode: "NG" });
+    }
   },
 };
 
--- backend/services/member.service.js
+++ backend/services/member.service.js
@@ -3,10 +3,10 @@ const db = require("../db");
 const service = {
   // 회원등록.
   register: async (data = {}) => {
-    const { user_id, user_pw, user_name } = data;
+    const { user_id, passwd, user_name } = data;
     const [result, schem] = await db.query(
       "insert into member(user_id, user_pw, user_name) values(?,?,?)",
-      [user_id, user_pw, user_name],
+      [user_id, passwd, user_name],
     );
     return result;
   },
